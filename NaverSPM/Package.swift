39d8662d446b1c3be83c3911c0083c04a5d4666f
namsoo5 <are34@naver.com>
2023-03-27 14:47:13 +0900

    폴더추가

--- NaverSPM/Package.swift
+++ NaverSPM/Package.swift
@@ -9,7 +9,8 @@ let package = Package(
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "NaverSPM",
-            targets: ["NaverSPM"]),
+            targets: ["NaverSPM"]
+        ),
     ],
     dependencies: [
         // Dependencies declare other packages that this package depends on.
@@ -20,9 +21,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "NaverSPM",
-            dependencies: []),
-        .testTarget(
-            name: "NaverSPMTests",
-            dependencies: ["NaverSPM"]),
+            dependencies: []
+        )
     ]
 )
